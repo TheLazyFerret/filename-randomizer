173f5fb39f02507f246d03bdcb0149721ced180f
TheLazyFerret <150300262+TheLazyFerret@users.noreply.github.com>
2026-07-20 23:37:11 +0100

    Update and improve argparse

--- src/argparse/argparse.cpp
+++ src/argparse/argparse.cpp
@@ -2,6 +2,9 @@
 
 #include "argparse/argparse.hpp"
 
+#include <cassert>
+#include <expected>
+#include <format>
 #include <string>
 #include <vector>
 
@@ -17,17 +20,52 @@ std::vector<std::string> argparse::argv_to_vec(int argc, char** argv) {
   return args;
 }
 
+std::expected<void, ParseError> argparse::parse_args_ops(ParsedArgs& parsed_args, const std::string& arg) {
+  if (arg == "-h" || arg == "--help") {
+    parsed_args.help = true;
+  } else if (arg == "-v" || arg == "--version") {
+    parsed_args.version = true;
+  } else if (arg == "-n" || arg == "--nonstop") {
+    parsed_args.nonstop = true;
+  } else if (arg == "-p" || arg == "--print-changes") {
+    parsed_args.print_changes = true;
+  } else {
+    return std::unexpected<ParseError>({arg, ParseErrorType::UnkownOption});
+  }
+  return {};
+}
+
 /// Parse the arguments.
 std::expected<ParsedArgs, ParseError> argparse::parse_args(const std::vector<std::string>& args) {
   ParsedArgs parsed_args;
   for (const auto& arg : args) {
-    if (arg == "-h" || arg == "--help") {
-      parsed_args.help = true;
-    } else if (arg == "-v" || arg == "--version") {
-      parsed_args.version = true;
+    if (arg.starts_with("-") || arg.starts_with("--")) {
+      const auto res = parse_args_ops(parsed_args, arg);
+      if (!res) return std::unexpected(res.error());
     } else {
-      return std::unexpected<ParseError>({arg, ParseErrorType::UnkownOption});
+      if (parsed_args.path.empty()) parsed_args.path = arg;
+      else return std::unexpected<ParseError>({arg, ParseErrorType::UnexpectedArgument});
     }
   }
+  if (parsed_args.path.empty()) {
+    return std::unexpected<ParseError>({"", ParseErrorType::NoPath});
+  }
   return parsed_args;
+}
+
+/// Return a formated string of the ParseError.
+std::string argparse::format_parse_error(const ParseError& err) {
+  switch (err.type) {
+  case ParseErrorType::UnkownOption:
+    return std::format("Unkown argument: {}", err.message);
+    break;
+  case ParseErrorType::UnexpectedArgument:
+    return std::format("Unexpected argument: {}", err.message);
+    break;
+  case ParseErrorType::NoPath:
+    return std::format("No path was suplied");
+    break;
+  default:
+    return "";
+  }
 }
--- src/argparse/argparse.hpp
+++ src/argparse/argparse.hpp
@@ -8,7 +8,7 @@
 
 namespace argparse {
 
-enum class ParseErrorType { UnkownOption };
+enum class ParseErrorType { UnkownOption, UnexpectedArgument, NoPath };
 
 struct ParseError {
   std::string message;
@@ -19,9 +19,14 @@ struct ParsedArgs {
   bool help = false;
   bool version = false;
   bool nonstop = false;
+  bool print_changes = false;
+  std::string path = "";
 };
+
+std::expected<void, ParseError> parse_args_ops(ParsedArgs&, const std::string&);
 
 std::vector<std::string> argv_to_vec(int argc, char** argv);
 std::expected<ParsedArgs, ParseError> parse_args(const std::vector<std::string>&);
+std::string format_parse_error(const ParseError&);
 
 } // namespace argparse
