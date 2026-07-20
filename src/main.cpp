352f307cbda8cc4bb72745f2cc533b93c024d891
TheLazyFerret <150300262+TheLazyFerret@users.noreply.github.com>
2026-07-20 23:39:57 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,16 @@
 /// Main program file.
 
 #include "utils/utils.hpp"
+#include "argparse/argparse.hpp"
 
 #include <cstdlib>
 #include <print>
 
-int main(/*int argc, char** argv*/) {
-  const auto result = utils::paths_in_directory("./test1");
-  if (!result) {
-    std::println("{}", result.error().message());
+int main(int argc, char** argv) {
+  const auto args_result = argparse::parse_args(argparse::argv_to_vec(argc, argv));
+  if (!args_result) {
+    std::println("{}", argparse::format_parse_error(args_result.error()));
     return EXIT_FAILURE;
   }
-  const auto result2 = utils::filter_regular_files(result.value());
-  if (!result2) {
-    std::println("{}", result2.error().message());
-    return EXIT_FAILURE;
-  }
-  const auto random_set = utils::generate_random_path_set(result2.value());
   return EXIT_SUCCESS;
 }
